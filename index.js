3e80ce19ca09aff973753920c19dcb27c89f55ed
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2023-11-30 16:51:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+JavaScript modules allow you to break up your code into separate files.
+
+This makes it easier to maintain the code-base.
+
+ES Modules rely on the import and export statements.
+
+ W3Schools Explained all. go and check.
